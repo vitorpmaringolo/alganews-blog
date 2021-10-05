822fda1c3ea3acc91246e13025db72adc8c06d2a
Vitor Palhares Maringolo <vitor.pmaringolo@gmail.com>
2021-10-04 23:52:41 -0300

    11.9. Styled Components no Next

---
+++ components/Header.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components"
+
+export default function Header() {
+    return (
+        <>
+            <Title>Oi eu sou um header</Title>
+        </>
+    )
+}
+
+const Title = styled.h1`
+    color: blue;
+`
