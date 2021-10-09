8db8877643371a2d62c676faf2e6b243802583cd
Vitor Palhares Maringolo <vitor.pmaringolo@gmail.com>
2021-10-09 16:02:57 -0300

    11.27. Adicionando animação

--- components/PostCard.tsx
+++ components/PostCard.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 import { transparentize } from "polished";
 import styled from "styled-components";
 import { Post } from "vitorpmaringolo-sdk";
@@ -10,20 +11,22 @@ interface PostCardProps {
 export default function PostCard(props: PostCardProps) {
   const { post } = props;
   return (
-    <Wrapper>
-      <Thumbnail bg={post.imageUrls.small} />
-      <Info>
-        <Editor>
-          <EditorImage
-            src={post.editor.avatarUrls.small}
-            width={64}
-            height={64}
-          />
-        </Editor>
-        <PublishDate>há 3 dias</PublishDate>
-        <Title>{post.title}</Title>
-      </Info>
-    </Wrapper>
+    <Link href={`/posts/${post.id}/${post.slug}`} passHref>
+      <Wrapper>
+        <Thumbnail bg={post.imageUrls.small} />
+        <Info>
+          <Editor>
+            <EditorImage
+              src={post.editor.avatarUrls.small}
+              width={64}
+              height={64}
+            />
+          </Editor>
+          <PublishDate>há 3 dias</PublishDate>
+          <Title>{post.title}</Title>
+        </Info>
+      </Wrapper>
+    </Link>
   );
 }
 
@@ -83,7 +86,7 @@ const Title = styled.h2`
   font-size: 14px;
 `;
 
-const Wrapper = styled.div`
+const Wrapper = styled.a`
   position: relative;
   min-height: 256px;
   background-color: ${(p) => p.theme.activeElementBackground};
@@ -91,4 +94,28 @@ const Wrapper = styled.div`
   border-radius: ${(p) => p.theme.borderRadius};
   box-shadow: 0 3px 6px
     ${(p) => transparentize(0.9, p.theme.activeElementForeground)};
+
+  transition: 0.25s ease;
+
+  * {
+    transition: 0.25s ease;
+  }
+
+  &:hover,
+  &:focus {
+    background-color: ${(p) => p.theme.primaryBackground};
+    box-shadow: 0 0 0 4px
+      ${(p) => transparentize(0.7, p.theme.primaryBackground)};
+
+    outline: none;
+
+    * {
+      color: ${(p) => p.theme.primaryForeground};
+    }
+
+    ${Thumbnail} {
+      height: 100%;
+      opacity: 0.1;
+    }
+  }
 `;
